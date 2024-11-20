20c1f1107a5f83e5d8867060a1f69fc3434ba4e1
ankitmicrogit <ankitmicro12345@gmail.com>
2024-11-21 03:29:00 +0530

    Improve error message for missing attendance record to include the specified date

--- src/services/user.js
+++ src/services/user.js
@@ -56,7 +56,7 @@ const logout = async (req, res) => {
     if (!attendance) {
       return {
         status: 404,
-        message: "Attendance record for the specified date not found",
+        message: `Attendance record for ${parsedDate} date not found`,
       };
     }
 
